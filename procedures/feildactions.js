7fb11277c3b04ca2fa4267c5b3542038e147aaa2
babak <babak.zarrinbal@gmail.com>
2019-12-08 18:48:42 +0330

    minor debug

--- procedures/feildactions.js
+++ procedures/feildactions.js
@@ -4,27 +4,31 @@ const { getPositions } = require("lodash-bzextras");
 /**
  * @param {object} data origins and targets data
  * @param {object} config procedure configuration
- * @param {string} config.parent location to insert new feild( array or object )
- * @param {string?} config.name name of the feild if parent is object
- * @param {*?} config.data data of the new feild
+ * @param {string} config.parent location to insert new field( array or object )
+ * @param {string?} config.name name of the field if parent is object
+ * @param {*?} config.data data of the new field
  */
-const feildAdd = function feildAdd(data, config) {
+const fieldAdd = function fieldAdd(data, config) {
   try {
     if (!config.parent) throw new Error("parent can't be empty");
-    let feildlist = getPositions(config.parent);
-    if (!feildlist.length) throw new Error("Parent path is incorrect");
-    feildlist.forEach(p => {
+    let fieldlist = getPositions(data, config.parent);
+    if (!fieldlist.length) throw new Error("Parent path is incorrect");
+    let targetdata = config.data === undefined ? {} : config.data;
+    try {
+      targetdata = JSON.parse(targetdata);
+    } catch (e) {}
+    fieldlist.forEach(p => {
       let parent = _.get(data, p);
-      let targetdata = config.data === undefined ? {} : config.data;
       if (typeof parent != "object")
         throw new Error("Parent must be object or array @ ", p);
       if (Array.isArray(parent)) parent.push(targetdata);
-      else parent[config.name || "undefined"] = targetdata;
+      else if (parent) parent[config.name || "undefined"] = targetdata;
+      else if (p.indexOf(".") == -1) data[p] = targetdata;
     });
   } catch (e) {
     return e;
   }
-  return ;
+  return;
 };
 
 /**
@@ -33,19 +37,19 @@ const feildAdd = function feildAdd(data, config) {
  * @param {string} config.from location of origin data
  * @param {string} config.to location of target data
  */
-const feildCopy = function feildCopy(data, config) {
+const fieldCopy = function fieldCopy(data, config) {
   try {
     if (!config.from || !config.to)
       throw new Error("config.from and config.to can't be empty");
-    let from = getPositions(config.from);
-    let to = getPositions(config.to);
-    if (!from.length || !to.length || to.length > 1)
+    let from = getPositions(data, config.from);
+    let to = getPositions(data, config.to);
+    if (!from.length || !to.length)
       throw new Error("Error in config.from or config.to path");
     let resultdata =
       from.length == 1
         ? _.cloneDeep(_.get(data, config.from))
         : from.map(p => _.cloneDeep(_.get(data, p)));
-    _.set(data, targetpath, resultdata);
+    to.forEach(tar => _.set(data, tar, resultdata));
   } catch (e) {
     return e;
   }
@@ -55,67 +59,79 @@ const feildCopy = function feildCopy(data, config) {
 /**
  * @param {object} data origins and targets data
  * @param {object} config procedure configuration
- * @param {string} config.path location of feild to remove
+ * @param {string} config.path location of field to remove
  */
-const feildRemove = function feildRemove(data, config) {
+const fieldRemove = async function fieldRemove(data, config) {
   try {
-  let feilds = getPositions(config.path);
-  if(!feilds.length) throw new Error('no such path exists');
-  feilds.forEach(f=>{
-    _.omit(data, f);
-  })
+    let fields = getPositions(data, config.path);
+    if (!fields.length) throw new Error("no such path exists");
+    let tdata = _.omit(data, fields);
+    Object.keys(data).forEach(k => {
+      if (!tdata[k]) return delete data[k];
+      data[k] = tdata[k];
+    });
+    // fields.forEach(f => {
+    //   _.omit(data, f);
+    // });
   } catch (e) {
     return e;
   }
-  return ;
+  return;
 };
 
 /**
  * @param {object} data origins and targets data
  * @param {object} config procedure configuration
- * @param {string} config.path location of feilds to rename
- * @param {string} config.name new name of feilds
+ * @param {string} config.path location of fields to rename
+ * @param {string} config.name new name of fields
  */
-const feildRename = function feildRename(data, config) {
+const fieldRename = function fieldRename(data, config) {
   try {
-    let feilds = getPositions(config.path);
-    if(!feilds.length) throw new Error('no such path exists');
-    feilds.forEach(f=>{
-      let newPath = f.slice(0,f.lastIndexOf('.')+1)
-      _.set(data,newPath,_.cloneDeep(_.get(data,f)));
-      _.omit(data,f);
-    })
-    } catch (e) {
-      return e;
-    }
-    return ;
+    let fields = getPositions(data, config.path);
+    if (!fields.length) throw new Error("no such path exists");
+    fields.forEach(f => {
+      let newPath = f.slice(0, f.lastIndexOf(".") + 1) + config.name;
+      _.set(data, newPath, _.cloneDeep(_.get(data, f)));
+      _.omit(data, f);
+      let tdata = _.omit(data, f);
+      Object.keys(data).forEach(k => {
+        if (!tdata[k]) return delete data[k];
+        data[k] = tdata[k];
+      });
+    });
+  } catch (e) {
+    return e;
+  }
+  return;
 };
 
 /**
  * @param {object} data origins and targets data
  * @param {object} config procedure configuration
- * @param {string} config.path location of feilds to change content
+ * @param {string} config.path location of fields to change content
  * @param {string} config.data new data to set
  */
-const feildSetContent = function feildSetContent(data, config) {
+const fieldSetContent = function fieldSetContent(data, config) {
   try {
-    let feilds = getPositions(config.path);
-    if(!feilds.length) throw new Error('no such path exists');
-    feilds.forEach(f=>{
-      _.set(data,f,_.cloneDeep(config.data));
-    })
-    } catch (e) {
-      return e;
-    }
-    return ;
-
+    let fields = getPositions(data, config.path);
+    if (!fields.length) throw new Error("no such path exists");
+    let contentdata;
+    try {
+      contentdata = JSON.parse(config.data);
+    } catch (e) {}
+    fields.forEach(f => {
+      _.set(data, f, _.cloneDeep(contentdata));
+    });
+  } catch (e) {
+    return e;
+  }
+  return;
 };
 
-
 module.exports = {
-  feildAdd,
-  feildCopy,
-  feildRemove,
-  feildRename,
-  feildSetContent
+  fieldAdd,
+  fieldCopy,
+  fieldRemove,
+  fieldRename,
+  fieldSetContent
 };
