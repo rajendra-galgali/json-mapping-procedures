c09fe9cf7df98b13548aed3fea294bcd8ca49694
babak <babak.zarrinbal@gmail.com>
2019-12-08 12:04:26 +0330

    initialization commit

--- index.js
+++ index.js
@@ -1,20 +1,4 @@
 const _ = require('lodash');
 const  {getPositions}  = require('lodash-bzextras');
 
-let a = { a: [
-  {
-    a:1,b:3
-  },
-  {
-    a:1,b:3
-  },
-  {
-    a:1,b:3
-  },
-  {
-    a:1,b:3
-  }
-
-] };
-
 console.log(getPositions(a,"a[].a"))
